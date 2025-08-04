c762bfc00470fa823d39e7ad69097ecb8adbb79d
Muralee2 <muralimvkaws1@gmail.com>
2025-08-04 11:14:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,11 +52,11 @@ resource "google_compute_firewall" "internal_traffic" {
 }
 
 resource "google_container_cluster" "gke_cluster" {
-  name     = "secure-gke"
+  name     = "secure-gke-cluster"
   location = var.region
 
-  network    = google_compute_network.vpc_network.name
-  subnetwork = google_compute_subnetwork.subnet.name
+  network    = google_compute_network.vpc_network.self_link
+  subnetwork = google_compute_subnetwork.subnet.self_link
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -68,33 +68,24 @@ resource "google_container_cluster" "gke_cluster" {
   }
 
   ip_allocation_policy {
-    cluster_secondary_range_name  = "pods"
-    services_secondary_range_name = "services"
+    cluster_secondary_range_name  = "pods-range"
+    services_secondary_range_name = "services-range"
   }
 
   master_authorized_networks_config {
     cidr_blocks {
       cidr_block   = "0.0.0.0/0"
-      display_name = "all"
+      display_name = "All networks"
     }
   }
 
-  logging_service    = "logging.googleapis.com/kubernetes"
-  monitoring_service = "monitoring.googleapis.com/kubernetes"
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "default-node-pool"
-  cluster    = google_container_cluster.gke_cluster.name
-  location   = var.region
-  node_count = 2
-
+  # ✅ Use custom service account for nodes
   node_config {
-    machine_type   = "e2-medium"
-    service_account = var.gke_node_sa_email # ✅ NEW LINE
+    machine_type = "e2-medium"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
+    service_account = var.gke_node_sa_email  # <-- critical
   }
 }
 
